dd1180e0c8e94cd9f42a5f6870c680a876542087
Aahnik Daw <aahnikdaw@gmail.com>
2020-07-15 12:47:21 +0530

    updated work dong

--- pollsite/home/views.py
+++ pollsite/home/views.py
@@ -7,7 +7,12 @@ import yaml
 def index(request):
     with open('/home/aahnik/Documents/django-polling_site/pollsite/home/homePage.yaml', 'r') as file:
         homePageElements = yaml.full_load(file)
-
+    stats = {
+        'forks': 10,
+        'choices': 100,
+        'participants': 200,
+        'questions': 30
+    }
     team = TeamMember.objects.order_by('title')
-    context = {'hpe': homePageElements, 'team': team}
+    context = {'hpe': homePageElements, 'team': team, 'stats': stats}
     return render(request, 'home/index.html', context)
